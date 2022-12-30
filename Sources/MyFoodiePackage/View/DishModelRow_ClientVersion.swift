944968186340ee0f8dbe185ae6e039598a440672
Lillo <lillofriscia@gmail.com>
2022-12-30 12:14:51 +0100

    Bug fix import UiKit - MyPackView (0.1.0)

---
+++ Sources/MyFoodiePackage/View/DishModelRow_ClientVersion.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Calogero Friscia on 28/12/22.
+//
+
+import SwiftUI
+
+
+public struct DishModelRow_ClientVersion: View {
+    
+    let item: DishModel
+    
+   public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+/*
+struct DishModelRow_ClientVersion_Previews: PreviewProvider {
+    static var previews: some View {
+        DishModelRow_ClientVersion()
+    }
+} */
